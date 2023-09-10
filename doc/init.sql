e579fcf3e913adfb2560dc85d148751c528d357e
caizhaolin <caizl@cai.com>
2023-09-11 00:00:55 +0800

    feat 建表语句

---
+++ doc/init.sql
@@ -0,0 +1,39 @@
+create table job_market(
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='人才市场表';
+
+create table job_convention_center(
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='会场表';
+
+create table job_convention_center_company(
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='会场-企业表';
+
+create table company(
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='企业表';
+
+create table company_position(
+
+  )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='企业职位表';
+
+create table company_position_video(
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='企业职位视频表';
+
+create table job_market_account(
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='人才市场账号表';
+
+create table company_account(
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='企业账号表';
+
+create table interview(
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='面试记录表';
+
+create table recorded_broadcast(
+
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='录播记录表';
